5e137b20a822379de6b98d4dbc181112483abfc7
raulsorrentino <sorrentinoraul@gmail.com>
2024-04-08 00:48:11 +0200

    start spring mvc in memory

--- src/main/java/com/raulsorrentino/contentcalendar/Application.java
+++ src/main/java/com/raulsorrentino/contentcalendar/Application.java
@@ -2,10 +2,6 @@ package com.raulsorrentino.contentcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/com/raulsorrentino/contentcalendar/controller/ContentController.java
@@ -0,0 +1,52 @@
+package com.raulsorrentino.contentcalendar.controller;
+
+import com.raulsorrentino.contentcalendar.model.Content;
+import com.raulsorrentino.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.delete(id);
+    }
+}
