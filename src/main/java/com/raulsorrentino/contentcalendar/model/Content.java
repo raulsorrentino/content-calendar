f75cb7373ce388e5d139769d2003b7dd4fd603bf
raulsorrentino <sorrentinoraul@gmail.com>
2024-04-08 15:06:46 +0200

    validation on title of Content

--- src/main/java/com/raulsorrentino/contentcalendar/model/Content.java
+++ src/main/java/com/raulsorrentino/contentcalendar/model/Content.java
@@ -1,9 +1,12 @@
 package com.raulsorrentino.contentcalendar.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+        @NotBlank
         String title,
         String desc,
         Status status,
